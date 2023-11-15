aa571844a1e5c2cec2af92df826d14d8bfb8a748
Dmitry Demidovich <d.demidovich@gpsolutions.com>
2023-11-15 20:42:21 +0400

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,13 @@
+import './globals.css'
+import {PropsWithChildren} from "react";
+
+
+export default function RootLayout({
+  children,
+}: PropsWithChildren) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
